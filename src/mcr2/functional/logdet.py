3fb1c97c030b00dc258fc7afa71a067f330d622a
Druv Pai <druv@druvpai.com>
2022-07-27 10:43:41 -0700

    sped up logdet using svdvals -- more stable too

--- src/mcr2/functional/logdet.py
+++ src/mcr2/functional/logdet.py
@@ -1,22 +1,23 @@
 import torch
 
 
-def logdet(H):
+def logdet(H, min_sv=1e-8):
     """
-    Takes the log determinant of a (possible batch of) real symmetric matrix.
+    Takes the log determinant of a (possible batch of) real symmetric positive definite matrix.
     Args:
         H: A Hermitian matrix batch (*, D, D)
 
     Returns:
         The log determinant of H (*, )
     """
-    ld = torch.slogdet(H)
-    return ld[0] * ld[1]
+    singular_values = torch.linalg.svdvals(H)
+    thresholded_singular_values = torch.clamp(singular_values, min=min_sv, max=None)
+    return torch.sum(torch.log(thresholded_singular_values), dim=-1)
 
 
 def logdet_complex(H):
     """
-    Takes the log determinant of a (possible batch of) complex Hermitian matrix.
+    Takes the log determinant of a (possible batch of) complex positive definite Hermitian matrix.
     Args:
         H: A Hermitian matrix batch (*, D, D)
 
